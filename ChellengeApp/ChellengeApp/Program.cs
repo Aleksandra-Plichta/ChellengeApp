0ca10126f63bf75e9bb066c099593d8bc05135df
Aleksandra-Plichta <aleksandra_plichta01@wp.pl>
2025-01-24 14:43:11 +0100

    Zadanie domowe dzień 4

--- ChellengeApp/ChellengeApp/Program.cs
+++ ChellengeApp/ChellengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zadanie domowe dzień 4
+
+using System.ComponentModel.Design;
+
+var name = "Magda";
+var sex = "female";
+var age = 29;
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
