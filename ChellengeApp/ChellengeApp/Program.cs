2916ce99b40ec0f777b6b360805f60419dfe61d0
Aleksandra-Plichta <aleksandra_plichta01@wp.pl>
2025-01-29 01:46:18 +0100

    Zadanie domowe dzień 6

--- ChellengeApp/ChellengeApp/Program.cs
+++ ChellengeApp/ChellengeApp/Program.cs
@@ -1,16 +1,47 @@
-﻿// Zadanie domowe dzień 5
+﻿// Zadanie domowe dzień 6
 
-int number = 20010701;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-int[] digits = new int[10];
+using ChellengeApp;
 
-foreach (var letter in letters)
+Employee employee1 = new Employee("Aureliusz", "Mazur", 44);
+Employee employee2 = new Employee("Agnieszka", "Ziółkowska", 35);
+Employee employee3 = new Employee("Ambroży", "Tomaszewski", 37);
+
+employee1.AddScore(3);
+employee1.AddScore(5);
+employee1.AddScore(6);
+employee1.AddScore(2);
+employee1.AddScore(7);
+
+employee2.AddScore(8);
+employee2.AddScore(10);
+employee2.AddScore(2);
+employee2.AddScore(4);
+employee2.AddScore(2);
+
+employee3.AddScore(10);
+employee3.AddScore(10);
+employee3.AddScore(8);
+employee3.AddScore(8);
+employee3.AddScore(3);
+
+
+List<Employee> employees = new List<Employee>()
 {
-    digits[letter - '0'] += 1;
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        maxResult = employee.Result;
+        employeeWithMaxResult = employee;
+    }
 }
 
-for (int i = 0; i < digits.Length; i++)
-{
-    Console.WriteLine(i.ToString() + " => " + digits[i]);
-}
+
+Console.WriteLine("Pracownik z największą sumą ocen");
+Console.WriteLine($"{employeeWithMaxResult.Name} {employeeWithMaxResult.Surname} lat {employeeWithMaxResult.Age} uzyskał wynik wynik: {employeeWithMaxResult.Result}");
